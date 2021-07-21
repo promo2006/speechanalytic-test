eade58d9703222cda94d92bf62978f5199fa276d
Emer Isau <emer.isau@gmail.com>
2021-07-21 10:49:33 -0500

    Testing submodule commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ describe('Launching integration tests', () => {
     });
     */
     common.importTest('AuthService works', './integration/auth.test.js');
-    common.importTest('SocketIO works', './integration/io.test.js');
+   // common.importTest('SocketIO works', './integration/io.test.js');
    // common.importTest('Delete utterances works', './integration/server/services/utterance.test.js');
 });
 
