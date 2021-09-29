69ddd75b40f9cbee465bfee80ff345f4a5acf58f
rgpedro <rgpedro@hotmail.com>
2021-09-29 15:53:05 -0500

    Add generic query and request tests

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
+var express = require('express');
+let app = express();
+
 //let app = null
 var common = require("./common");
 
-describe('Launching unit tests', () => {
-    common.importTest('Server>shared>arrays.shared', './unit/server/shared/arrays.shared.test.js');
-    common.importTest('Server>implementation>audio-transfer.implementation', './unit/server/implementation/audio-transfer.implementation.test.js');
-    common.importTest('Server>implementation>transcriptions.implementation', './unit/server/implementation/transcriptions.implementation.test.js');
+let serverSrcPath = common.serverSrcPath;
+let mssqlInit = require( `${serverSrcPath}/db/mssql.init`);
 
-    common.importTest('Server>services>linguistic.service', './unit/server/services/linguistic.service.test.js');
+describe('Launching unit tests', () => {
+    //common.importTest('Server>shared>arrays.shared', './unit/server/shared/arrays.shared.test.js');
+    common.importTest('Server>shared>arrays.shared', './unit/server/shared/request.shared.test.js');
+    //common.importTest('Server>implementation>audio-transfer.implementation', './unit/server/implementation/audio-transfer.implementation.test.js');
+    //common.importTest('Server>implementation>semantic-query.implementation', './unit/server/implementation/semantic-query.implementation.test.js');
+    //common.importTest('Server>implementation>transcriptions.implementation', './unit/server/implementation/transcriptions.implementation.test.js');
+
+    //common.importTest('Server>services>linguistic.service', './unit/server/services/linguistic.service.test.js');
 });
 
 
 describe('Launching integration tests', () => {
-   /* before(() => {
-        console.log("before all tests");
-        //app = require('../../dist/dev/app.server');
+    
+    before(() => {
+        //console.log("before all tests");
+        /*
+        return mssqlInit.MsSqlInit(app)
+        .then(_ => {
+            
+        });
+        */
     });
-
+    
     //beforeEach(() => {
     //   console.log("running something before each test");
     //});
     
-    
+    //common.importTest('Server>implementation>semantic-query.implementation', './unit/server/implementation/semantic-query.implementation.test.js');
+    /*
     common.importTest('Main page works', './example.test.js');
     common.importTest('AuthService works', './integration/auth.test.js');
     common.importTest('NLUService works', './integration/server/services/nlu.service.test.js');
