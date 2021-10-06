e9efb5fe6205cad168a56a36f047e7c75e1bd6ce
rgpedro <rgpedro@hotmail.com>
2021-10-06 14:49:07 -0500

    Add create rasa nlu test

--- unit/server/implementation/rasa-service.implementation.test.js
+++ unit/server/implementation/rasa-service.implementation.test.js
@@ -12,6 +12,40 @@ let rasaServiceImplementation = require(`${serverSrcPath}/implementation/rasa-se
 let rasaServiceImpl = new rasaServiceImplementation.RASAServiceImplementation();
 
 // 
+describe('AddApp', () => {
+    it('AddApp: Creating a rasa nlu', () => {
+        let nlu ={
+            culture: values.newNlu.culture, 
+            description: "test-1323", 
+            importNLU: false, 
+            importNLUFrom: null, 
+            importNLULoaded: false, 
+            name: faker.random.uuid(),
+            vcc: 'collegeocc'
+        }
+
+        return rasaServiceImpl.AddApp(null, null, nlu)
+        .then(result => {
+            console.log('Created nlu:' + nlu.name)
+            console.log(result);
+            // Verificamos que tenga el status de éxito de la función
+            /*
+            result.status.should.to.equal(3);
+            result.jobId.should.to.equal(99999);
+            should.equal(result.errorFunction, null);
+            should.equal(result.errorMessage, null);
+            should.equal(result.errorDetail, null);
+            */
+            //result.jobId.should.to.be.a('number', 99999);
+            //result.errorFunction.should.to.be.a('null');
+            //result.errorMessage.should.to.be.a('null');
+            //result.errorDetail.should.to.be.a('null');
+        });
+        // No colocamos catch porque provocaría un falso positivo en el test
+    });
+});
+
+// 
 describe('GetAppsList', () => {
     // 
     it('GetAppsList: Getting all nlus', () => {
